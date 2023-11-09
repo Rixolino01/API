74c48c170b5e72ff1ceb0fe8f1850ec90fd898c1
Rixolino01 <richardson.trizzino@gmail.com>
2023-11-09 20:13:52 -0300

    backend

--- Ifood_pet/pages/src/utils/ThemeContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-//Criando a variações entre os temas claro e escuros
-const ThemeContext = createContext(["light", () => {}]);
-
-export default ThemeContext;
